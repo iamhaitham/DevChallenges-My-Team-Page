38d080583c6c558a7d65881099c6506995fa50f2
iamhaitham <iamhaitham2011@gmail.com>
2022-06-08 22:40:23 +0300

    Change a variable name in the script

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-let peopleWrapperCollection = document.querySelectorAll('.personWrapper > article > span');
+let peopleWrapperSpans = document.querySelectorAll('.personWrapper > article > span');
 
-peopleWrapperCollection.forEach(element => {
+peopleWrapperSpans.forEach(element => {
     element.style.position = 'absolute';
     element.style.height = element.previousElementSibling.clientHeight + 'px';
 });
