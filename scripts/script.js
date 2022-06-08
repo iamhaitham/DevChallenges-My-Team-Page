f09e54021ca27de5e57adb4691eea404f7b6dc03
iamhaitham <iamhaitham2011@gmail.com>
2022-06-08 22:58:04 +0300

    Add a peopleWrapperSpans region in the script

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
+// #region peopleWrapperSpans
 let peopleWrapperSpans = document.querySelectorAll('.personWrapper > article > span');
 
 peopleWrapperSpans.forEach(element => {
     element.style.position = 'absolute';
     element.style.height = element.previousElementSibling.clientHeight + 'px';
 });
+// #endregion
+
+
