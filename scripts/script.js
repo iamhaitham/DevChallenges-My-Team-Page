5df04ab106dd22848d2f9dfa0253a50f0f6ec525
iamhaitham <iamhaitham2011@gmail.com>
2022-06-11 00:05:14 +0300

    Remove unnecessary functions from the script file

--- scripts/script.js
+++ scripts/script.js
@@ -6,22 +6,3 @@ peopleWrapperSpans.forEach(element => {
     element.style.height = element.previousElementSibling.clientHeight + 'px';
 });
 // #endregion
-
-
-// #region media query helper functions
-function maxWidthQuery(maxWidth) {
-    return window.matchMedia(`(max-width: ${maxWidth}px)`);
-}
-
-function isMaxWidthMatches(maxWidth) {
-    return maxWidthQuery(maxWidth).matches;
-}
-
-function minWidthQuery(minWidth) {
-    return window.matchMedia(`(min-width: ${minWidth}px)`);
-}
-
-function isMinWidthMatches(minWidth) {
-    return minWidthQuery(minWidth).matches;
-}
-// #endregion
