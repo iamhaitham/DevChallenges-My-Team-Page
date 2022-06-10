f5bd35c1b4595966336b6be9915ecd3acc39fd42
iamhaitham <iamhaitham2011@gmail.com>
2022-06-11 00:03:56 +0300

    Get rid of changing margin-top from javascript, and thus no reloading is required anymore

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-window.onresize = () => location.reload();
-
 // #region peopleWrapperSpans
 let peopleWrapperSpans = document.querySelectorAll('.personWrapper > article > span');
 
@@ -25,24 +23,5 @@ function minWidthQuery(minWidth) {
 
 function isMinWidthMatches(minWidth) {
     return minWidthQuery(minWidth).matches;
-}
-// #endregion
-
-
-// #region peopleWrappers
-let peopleWrappers = Array.from(document.querySelectorAll('.personWrapper'));
-
-if(isMaxWidthMatches(500)) {
-    peopleWrappers.filter((_, index) => index % 2 !== 0).forEach((element) => {
-        element.style.marginTop += 50 + 'px';
-    });
-} else if(isMinWidthMatches(501) && isMaxWidthMatches(768)) {
-    for(let i = 1; i < peopleWrappers.length; i += 3) {
-        if(i === 1){
-            peopleWrappers[1].style.marginTop += 50 + 'px';
-        } else {
-            peopleWrappers[i].style.marginTop += 100 + 'px';
-        }
-    }
 }
 // #endregion
